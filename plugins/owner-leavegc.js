dd454dbe7e687c21ba0f5cb116d2db5b16639121
Wajackoya <141861669+Wajackoya@users.noreply.github.com>
2024-12-15 00:16:51 +0300

    Update owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
-await conn.reply(id, 'https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] Goodbye everyone, the Bot says goodbye! (≧ω≦)ゞ*')
+await conn.reply(id, '*[❗𝐈𝐍𝐅𝐎❗] Goodbye everyone, the Bot says goodbye! (≧ω≦)ゞ*')
 await conn.groupLeave(id)}
 handler.command = /^(out|leavegc|leave|salirdelgrupo)$/i
 handler.group = true
