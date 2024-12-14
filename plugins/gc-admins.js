c09763da3cebeae09986d242e46c95d25ff7c698
Wajackoya <141861669+Wajackoya@users.noreply.github.com>
2024-12-15 00:06:06 +0300

    Update gc-admins.js

--- plugins/gc-admins.js
+++ plugins/gc-admins.js
@@ -4,7 +4,7 @@ const groupAdmins = participants.filter(p => p.admin)
 const listAdmin = groupAdmins.map((v, i) => `${i + 1}.@${v.id.split('@')[0]}`).join('\n▢ ')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 
-let text = `https://github.com/Khalid-official 
+let text = `https://github.com/Wajackoya
 ≡ *[❗𝐈𝐍𝐅𝐎❗] GROUP STAFF* _${groupMetadata.subject}_
 
 ┌─⊷ *ADMINS*
